0ddbe1398794320e38a4ab30e726b88c5b5252e9
Karoline de Souza Silva <karolinessilva@live.com>
2023-04-26 15:56:44 -0300

    Criação contexto

---
+++ GerenciamentoProduto/DataDbContext/GerenciamentoProdutoContext.cs
@@ -0,0 +1,31 @@
+﻿using GerenciamentoProduto.Domains;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata;
+
+namespace GerenciamentoProduto.DataDbContext
+{
+    public class GerenciamentoProdutoContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("store");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>()
+                .HasMany(e => e.Produtos)
+                .WithOne(e => e.Categoria)
+                .HasForeignKey(e=>e.CategoriaId)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
