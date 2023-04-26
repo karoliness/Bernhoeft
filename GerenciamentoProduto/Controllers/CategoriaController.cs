880229311aa356db17c055a29c91ca4e4e8b6665
Karoline de Souza Silva <karolinessilva@live.com>
2023-04-26 15:56:30 -0300

    Criação Controler

---
+++ GerenciamentoProduto/Controllers/CategoriaController.cs
@@ -0,0 +1,112 @@
+using FluentValidation;
+using GerenciamentoProduto.Domains;
+using GerenciamentoProduto.Domains.Dtos;
+using GerenciamentoProduto.Domains.Validations;
+using GerenciamentoProduto.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciamentoProduto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriaController : Controller
+    {
+        private readonly ILogger<CategoriaController> _logger;
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriaController(ILogger<CategoriaController> logger, ICategoriaService categoriaService)
+        {
+            _logger = logger;
+            _categoriaService = categoriaService;
+        }
+
+        [HttpGet("obter")]
+        public async Task<IActionResult> ObterTodasAsCategorias()
+        {
+            var categorias = await _categoriaService.ObterTodasAsCategorias();
+
+            if (!categorias.Any())
+            {
+                return StatusCode(404, MensagensErro.NaoEncontrado);
+
+            }
+            return StatusCode(202, BuscaCategoriaDto.ConverterCategoriasComProdutos(categorias));
+        }
+
+        [HttpGet("situacao/{situacao}")]
+        public async Task<IActionResult> ObterPorSituacao([FromRoute]bool situacao) 
+        {
+            var categorias = await _categoriaService.ObterCategoriasPorSituacaoAsync(situacao);
+
+            if (!categorias.Any())
+            {
+                return StatusCode(404, MensagensErro.NaoEncontrado);
+
+            }
+
+            return StatusCode(202, BuscaCategoriaDto.ConverterCategoriasComProdutos(categorias));
+        }
+
+        [HttpGet("nome/{nome}")]
+        public async Task<IActionResult> ObterPorNome([FromRoute] string nome)
+        {
+            var categorias = await _categoriaService.ObterCategoriasPorNomeAsync(nome);
+
+            if (!categorias.Any())
+            {
+                return StatusCode(404, MensagensErro.NaoEncontrado);
+
+            }
+            return StatusCode(202, BuscaCategoriaDto.ConverterCategoriasComProdutos(categorias));
+        }
+
+        [HttpPost("adicionar")]
+        public async Task<IActionResult> Adicionar(InsercaoCategoriaDto categoriaDto)
+        {
+            if (ValidacaoModel(categoriaDto, new InsercaoCategoriaDtoValidation()) is { } erros)
+            {
+                return StatusCode(403, erros);
+            }
+            var categoria = InsercaoCategoriaDto.ConverterDto(categoriaDto);
+            var categoriaAdicionada = await _categoriaService.AdicionarCategoriaAsync(categoria);
+            if(categoriaAdicionada == Guid.Empty)
+            {
+                return StatusCode(404, MensagensErro.NaoAdicionado);
+            }
+
+            return StatusCode(202, categoriaAdicionada);
+        }
+
+        [HttpPut("atualizar")]
+        public async Task<IActionResult> Atualizar(AlteracaoCategoriaDto categoriaDto)
+        {
+            if (ValidacaoModel(categoriaDto, new AlteracaoCategoriaDtoValidation()) is { } erros)
+            {
+                return StatusCode(403, erros);
+            }
+
+            var categoria = AlteracaoCategoriaDto.ConverterDto(categoriaDto);
+
+            var categoriaAdicionada = await _categoriaService.AlterarCategoriaAsync(categoria);
+
+            if (null == categoriaAdicionada)
+            {
+                return StatusCode(404, MensagensErro.NaoAdicionado);
+            }
+
+            return StatusCode(202, BuscaCategoriaDto.ConverterCategoriaComProdutos(categoria));
+        }
+
+        private List<string> ValidacaoModel<T>(T model, IValidator<T> validator) where T : class
+        {
+            var results = validator.Validate(model);
+
+            if (!results.IsValid)
+            {
+                return results.Errors.Select(x => x.ErrorMessage).ToList();
+            }
+            return null;
+        }
+
+    }
+}
---
+++ GerenciamentoProduto/Controllers/ProdutoController.cs
@@ -0,0 +1,126 @@
+﻿using FluentValidation;
+using GerenciamentoProduto.DataDbContext;
+using GerenciamentoProduto.Domains;
+using GerenciamentoProduto.Domains.Dtos;
+using GerenciamentoProduto.Domains.Validations;
+using GerenciamentoProduto.Services;
+using GerenciamentoProduto.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace GerenciamentoProduto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<CategoriaController> _logger;
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoController(ILogger<CategoriaController> logger, IProdutoService produtoService)
+        {
+            _logger = logger;   
+            _produtoService = produtoService;
+        }
+
+
+        [HttpGet("obter")]
+        public async Task<IActionResult> ObterTodosOsProdutos()
+        {
+            var produtos = await _produtoService.ObterTodosOsProdutos();
+
+            if (!produtos.Any())
+            {
+                return StatusCode(404, MensagensErro.NaoEncontrado);
+
+            }
+
+            var buscarProdutoDto = BuscaProdutoDto.ConverterProdutosComCategoria(produtos);
+            
+            return StatusCode(202, buscarProdutoDto);
+        }
+
+        [HttpGet("situacao/{situacao}")]
+        public async Task<IActionResult> ObterPorSituacao([FromRoute] bool situacao)
+        {
+            var produtos = await _produtoService.ObterProdutoPorSituacao(situacao);
+
+            if (!produtos.Any())
+            {
+                return StatusCode(404, MensagensErro.NaoEncontrado);
+
+            }
+            var buscarProdutoDto = BuscaProdutoDto.ConverterProdutosComCategoria(produtos);
+
+            return StatusCode(202, buscarProdutoDto);
+        }
+
+        [HttpGet("descricao/{descricao}")]
+        public async Task<IActionResult> ObterPorDescricao([FromRoute] string descricao)
+        {
+            var produtos = await _produtoService.ObterProdutoPorDescricao(descricao);
+
+            if (!produtos.Any())
+            {
+                return StatusCode(404, MensagensErro.NaoEncontrado);
+
+            }
+            var buscarProdutoDto = BuscaProdutoDto.ConverterProdutosComCategoria(produtos);
+
+            return StatusCode(202, buscarProdutoDto);
+        }
+
+        [HttpPost("adicionar")]
+        public async Task<IActionResult> Adicionar(InsercaoProdutoDto produtoDto)
+        {
+
+            if (ValidacaoModel(produtoDto, new InsercaoProdutoDtoValidation()) is {} erros)
+            {
+                return StatusCode(403, erros);
+            }
+            
+            var produto = InsercaoProdutoDto.ConverterDto(produtoDto);
+            var produtoAdicionado = await _produtoService.AdicionarProduto(produto);
+            if (produtoAdicionado == Guid.Empty)
+            {
+                return StatusCode(404, MensagensErro.NaoAdicionado);
+            }
+
+            return StatusCode(202, produtoAdicionado);
+        }
+
+        [HttpPut("atualizar")]
+        public async Task<IActionResult> Atualizar(AlteracaoProdutoDto produtoDto)
+        {
+            if (ValidacaoModel(produtoDto, new AlteracaoProdutoDtoValidation()) is { } erros)
+            {
+                return StatusCode(403, erros);
+            }
+
+            var produto = AlteracaoProdutoDto.ConverterDto(produtoDto);
+            var produtoAtualizado = await _produtoService.AlterarProduto(produto);
+            if (null == produtoAtualizado)
+            {
+                return StatusCode(404, MensagensErro.NaoAdicionado);
+            }
+
+            var buscarProdutoDto = BuscaProdutoDto.ConverterProdutoComCategoria(produto);
+
+            return StatusCode(202, buscarProdutoDto);
+        }
+
+
+        private List<string> ValidacaoModel<T>(T model, IValidator<T> validator) where T : class
+        {
+            var results = validator.Validate(model);
+
+            if (!results.IsValid)
+            {
+                return results.Errors.Select(x=>x.ErrorMessage).ToList();
+            }
+            return null;
+        }
+    }
+}
