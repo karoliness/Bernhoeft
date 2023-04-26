b5b97dd3bd5e65f03e88d435388753f5818435de
Karoline de Souza Silva <karolinessilva@live.com>
2023-04-26 15:57:08 -0300

    criação model

---
+++ GerenciamentoProduto/Domains/Categoria.cs
@@ -0,0 +1,22 @@
+﻿namespace GerenciamentoProduto.Domains
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+        }
+
+        public Categoria(string nome, bool situacao)
+        {
+            Nome = nome;
+            Situacao = situacao;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public bool Situacao { get; set; }
+        public List<Produto> Produtos { get; } = new();
+
+
+    }
+}
---
+++ GerenciamentoProduto/Domains/Produto.cs
@@ -0,0 +1,30 @@
+﻿using GerenciamentoProduto.Domains.Dtos;
+
+namespace GerenciamentoProduto.Domains
+{
+    public class Produto
+    {
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, string descricao, decimal preco, bool situacao, Categoria categoria)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+            Situacao = situacao;
+            Categoria = categoria;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public bool Situacao { get; set; }
+        public Guid CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; } = null!;
+
+
+    }
+}
